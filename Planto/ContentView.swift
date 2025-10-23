f26a8485c394f54e7ca3e0beab3c56fd4430014a
Rana <rana.binmadi@gmail.com>
2025-10-23 09:34:21 +0300

    Initial Commit

---
+++ Planto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Planto
+//
+//  Created by Rana on 01/05/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Planto/PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoApp.swift
+//  Planto
+//
+//  Created by Rana on 01/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
